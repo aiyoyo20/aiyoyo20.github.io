cd514cdfdf69ac1d9610e5d470de08e05892ea15
aiyoyo20 <aiyoyo20@gmail.com>
2021-06-30 08:25:26 +0800

    pelican static blog test

--- content/MongoDB.md
+++
@@ -1,4 +0,0 @@
-Title: MongoDB
-Category: Database
-tags: linux
-Date: 2020-9-03 10:20
--- githubblog.sh
+++
@@ -1,1 +0,0 @@
-make html && git add . && git commit -m "add blog" && git push -u origin master && cd output/ && git add . && git commit -m "add blog" && git push -u origin master && cd ..
